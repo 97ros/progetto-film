fbb4752094ad6f445e067bd9abd3a7b54efa3bb4
auroraaelia <a.elia7@studenti.poliba.it>
2025-09-09 23:43:32 +0200

    modifiche css definitive

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -200,8 +200,8 @@ function ProfilePage() {
     : [];
 
     return (
-        <Container className="mt-4" px={4} style={{ width: '100%', padding: '80px', paddingTop: null }}>
-            <Row className="align-items-center mb-4">
+        <Container className="mt-4" px={4} style={{ maxWidth: '1110px', width: '100%', paddingTop: null, position:'relative' }}>            
+        <Row className="align-items-center mb-4">
                 <Col xs="auto">
                     <Image src={isEditing ? (formData.profilePicture || 'https://via.placeholder.com/150') : (userProfile.profilePicture || 'https://via.placeholder.com/150')}
                     roundedCircle
