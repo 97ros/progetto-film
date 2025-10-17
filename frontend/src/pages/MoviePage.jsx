966abc92ee241c4f1ae6748499657028dcddbca0
Rossana <r.lasorsa@studenti.poliba.it>
2025-10-17 15:52:34 +0200

    Like

--- frontend/src/pages/MoviePage.jsx
+++ frontend/src/pages/MoviePage.jsx
@@ -236,7 +236,7 @@ function MoviePage() {
             <h3>Post su "{movieDetails.title}"</h3>
             {posts.length > 0 ? (
                 posts.map(post => {
-                    const isLiked = currentUser && post.likes.includes(currentUser._id);
+                    const isLiked = currentUser && post.likes?.some(likeId => likeId === currentUser?.Id);
                     return (
                     <Card key={post._id} className="mb-3">
                         <Card.Body>
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -351,7 +351,7 @@ function ProfilePage() {
             <h3>Post di {userProfile.username}</h3>
             {userPosts && userPosts.length > 0 ? (
                 userPosts.map(post => {
-                    const isLiked = currentUser && post.likes.includes(currentUser._id);
+                    const isLiked = currentUser && post.likes?.some(likeId => likeId === currentUser?.Id);
 
                     return (
                         <Card key={post._id} className="mb-3">
